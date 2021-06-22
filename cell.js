36fd665a119eba38241a0d52a6c14537019bca10
akbarhps <akbarputrasrg@gmail.com>
2021-06-22 10:14:21 +0700

    🧹 Decrease obstacle chance

--- cell.js
+++ cell.js
@@ -4,7 +4,7 @@
  * https://www.github.com/akbarhps
  */
 
-function Cell(y, x, obstacleChance = 0.5) {
+function Cell(y, x, obstacleChance = 0.4) {
     this.y = y;
     this.x = x;
     this.isObstacle = Math.random() < obstacleChance;
